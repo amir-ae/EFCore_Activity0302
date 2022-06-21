36c5d6bce46f9440cc7d28eab1a82c38863b8f4a
anton-prokhorov <45985422+anton-prokhorov@users.noreply.github.com>
2022-06-21 13:43:04 +0300

    Create items table

--- EFCore_DBLibrary/InventoryDbContext.cs
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCore_DBLibrary
 {
     public class InventoryDbContext : DbContext
     {
+        private static IConfigurationRoot? _configuration;
+
+        public DbSet<Item> Items { get; set; } = null!;
+
         public InventoryDbContext()
         {
 
         }
 
-        public InventoryDbContext(DbContextOptions options) : base(options)
+        public InventoryDbContext(DbContextOptions options)
+            : base(options)
         {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                _configuration = builder.Build();
+
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+            }
         }
     }
 }
