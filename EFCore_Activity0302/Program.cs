821791410e305226a1e64736054ed56a1fec5ee4
anton-prokhorov <45985422+anton-prokhorov@users.noreply.github.com>
2022-06-21 12:42:36 +0300

    Add project files.

---
+++ EFCore_Activity0302/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore_Activity0301;
+using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+BuildOptions();
+
+partial class Program
+{
+    private static IConfigurationRoot? _configuration;
+    private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder = new();
+
+    static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+    }
+}
---
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext()
+        {
+
+        }
+
+        public InventoryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
